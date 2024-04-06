03eec27f08ff5ddf08f52e722c8f5b1d65985560
Alexander <alexander.marval25@gmail.com>
2024-04-06 11:43:29 -0400

    Obtener un carro por id

--- car-dealership/src/cars/cars.controller.ts
+++ car-dealership/src/cars/cars.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+
+    private cars = ['Toyota', 'Honda', 'Jeep'];
+
     @Get()
     getAllCars(){
-        return ['Toyota', 'Honda', 'Jeep']
+        return this.cars;
+    }
+
+    @Get(':id')
+    getCarById( @Param('id') id: string) {
+        console.log({ id: +id })
+
+        if((+id + 1) == null ) return { id: null, message: "id is not a number" }
+        if(this.cars.length < +id) return { id: null, message: "id not exist" };
+        
+        return this.cars[+id];
     }
 }
