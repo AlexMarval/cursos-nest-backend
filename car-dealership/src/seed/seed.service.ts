f0463e0a7238a1bd76b5dde74c09a0bb300f54ed
Alexander Marval <alexander.marval25@gmail.com>
2024-04-19 11:09:11 -0400

    Implementering van diensinspuiting en ander dienste.

--- car-dealership/src/seed/seed.service.ts
+++ car-dealership/src/seed/seed.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { CarsService } from 'src/cars/cars.service';
+import { BrandsService } from 'src/brands/brands.service';
+import { CARS_SEED } from './data/cars.seed';
+import { BRANDS_SEED } from './data/brands.seed';
 //import { BRANDS_SEED } from './data/brands.seed';
 //import { CARS_SEED } from './data/cars.seed';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService,
+  ) {}
+
   populateDB() {
     //BRANDS_SEED
     //CARS_SEED
+
+    this.carsService.fillCarsWithSeedData(CARS_SEED);
+    this.brandsService.fillBrandsWithSeedData(BRANDS_SEED);
     return `Execute seed successful`;
   }
 }
